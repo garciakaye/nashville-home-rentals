bb82cabefc90a3844feedda4c2e6fdfb047ddaa8
kaye garcia <kayemgarcia@gmail.com>
2021-12-28 17:22:41 -0600

    Added username validation

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,14 @@ class UsersController < ApplicationController
 
     def create
         @user = User.new(user_params)
+
+        if @user.valid?
+            @user.save
+            @token = JWT.encode({user_id: @user.id}, "yerrr")
+            render json: { user: @user, token: @token}, status: :created 
+        else
+            render json: { error: @user.errors.full_messages }, status: :not_acceptable
+        end
     end
 
     private
