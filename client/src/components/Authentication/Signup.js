2a6915ed456ef3ef17fffc2bbbc2b29a4bd4b267
kaye garcia <kayemgarcia@gmail.com>
2022-01-03 12:30:32 -0600

    Created handleSubmit function in Signup component

--- client/src/components/Authentication/Signup.js
+++ client/src/components/Authentication/Signup.js
@@ -1,11 +1,37 @@
-import React from 'react'
+import React, { useState } from 'react';
 
 function Signup() {
-    return (
-        <div>
-            
-        </div>
-    )
+	const [username, setUsername] = useState('');
+	const [password, setPassword] = useState('');
+
+function handleSubmit(e){
+	e.preventDefault();
+
+	const strongParams = {
+		user: {
+			username,
+			password
+		}
+	}
+}
+
+
+  return (
+    <div>
+			<h1>Create Account</h1>
+      <form onSubmit={ handleSubmit }>
+				<div>
+					<label htmlFor="username">Username: </label>
+					<input type="text" name="username" id="username" value={ username } onChange={ e => setUsername(e.target.value)}/>
+				</div>
+				<div>
+					<label htmlFor="password">Password: </label>
+					<input type="password" name="password" id="password" value={ password } onChange={ e => setPassword(e.target.value)} />
+				</div>
+				<input type="submit" value="Create Account" />
+      </form>
+  	</div>
+  )
 }
 
 export default Signup
