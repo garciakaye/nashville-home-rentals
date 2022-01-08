d3cdefdcbec97057a4f9e426c477280167e4c147
kaye garcia <kayemgarcia@gmail.com>
2022-01-08 16:35:45 -0600

    Added show action in ListingsController

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -6,7 +6,14 @@ class ListingsController < ApplicationController
 	end
 	
 	def show
+		@listing = find(params[:id])
 		render json: @listing
 	end
+
+	# private
+
+	# def find_listing
+	# 	@listing = Listing.find_by_id(params[:id])
+	# end
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,13 +7,15 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 puts "Seeding data..."
 
-Review.create(comment: "Beautifully designed and near all the best restaurants and bars!", rating: 5, listing_id: 7)
-Review.create(comment: "Great place for large groups", rating: 4, listing_id: 8)
-Review.create(comment: "Best experience I've had! The hosts were amazing", rating: 4, listing_id: 9)
 
-Listing.create(image: "https://a0.muscache.com/im/pictures/4dfd9f68-1216-4656-85ab-23cb028adec9.jpg", name: "Nashville Greenhaus", price: 500)
-Listing.create(image: "https://a0.muscache.com/im/pictures/b56b68b0-0d5c-4645-989c-a8002377bd13.jpg", name: "Luxury Stay", price: 800)
-Listing.create(image: "https://a0.muscache.com/im/pictures/miso/Hosting-51086566/original/857b3c5c-1ba9-4150-a030-923bc0f59a98.jpeg", name: "Modern Luxury", price: 1100)
+
+listing1 = Listing.create(image: "https://a0.muscache.com/im/pictures/4dfd9f68-1216-4656-85ab-23cb028adec9.jpg", name: "Nashville Greenhaus", price: 500)
+listing2 = Listing.create(image: "https://a0.muscache.com/im/pictures/b56b68b0-0d5c-4645-989c-a8002377bd13.jpg", name: "Luxury Stay", price: 800)
+listing3 = Listing.create(image: "https://a0.muscache.com/im/pictures/miso/Hosting-51086566/original/857b3c5c-1ba9-4150-a030-923bc0f59a98.jpeg", name: "Modern Luxury", price: 1100)
+
+Review.create(comment: "Beautifully designed and near all the best restaurants and bars!", rating: 5, listing_id: listing1.id)
+Review.create(comment: "Great place for large groups", rating: 4, listing_id: listing2.id)
+Review.create(comment: "Best experience I've had! The hosts were amazing", rating: 4, listing_id: listing3.id)
 
 
 
