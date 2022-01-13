007f1883207d0a86c7df25476501178d15ad7f74
kaye garcia <kayemgarcia@gmail.com>
2022-01-12 22:33:18 -0600

    Cleaned up rails code

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,52 +1,52 @@
 class ReviewsController < ApplicationController
 
-    def index
-        reviews = Review.all
-        render json: reviews, include: :listing
-    end
+	def index
+		reviews = Review.all
+		render json: reviews, include: :listing
+	end
     
-    def show
+  def show
 		find_review
 		render json: @review, include: :listing
 	end
 
-    def create
-        @review = Review.new(review_params)
-        
-        if @review.save
-            render json: @review, status: :created, location: @review
-        else
-            render json: @review.errors, status: :unprocessable_entity
-        end
-    end
+	def create
+		@review = Review.new(review_params)
+		
+		if @review.save
+				render json: @review, status: :created, location: @review
+		else
+				render json: @review.errors, status: :unprocessable_entity
+		end
+	end
 
-    def update
-        @review = Review.find_by(id:params[:id])
-        if @review
-            @review.update(review_params)
-            render json: @review, status: :accepted
-        else
-            render json: {error: "review not found"}, status: :not_found
-        end
-    end
+	def update
+		find_review
+		if @review
+				@review.update(review_params)
+				render json: @review, status: :accepted
+		else
+				render json: {error: "review not found"}, status: :not_found
+		end
+	end
 
-    def destroy
-        @review = Review.find_by(id:params[:id])
-        if @review
-            @review.destroy
-            head :no_content
-        else
-            render json: {error: "review not found"}, status: :not_found
-        end
-    end
+	def destroy
+		find_review
+		if @review
+				@review.destroy
+				head :no_content
+		else
+				render json: {error: "review not found"}, status: :not_found
+		end
+	end
 
-    private
+  private
 
-    def find_review
-        @review = Review.find_by_id(params[:id])
-    end
+	def find_review
+		@review = Review.find_by_id(params[:id])
+	end
 
-    def review_params
-        params.require(:review).permit(:comment, :listing_id)
-    end
+	def review_params
+		params.require(:review).permit(:comment, :listing_id)
+	end
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,19 +1,19 @@
 class SessionsController < ApplicationController
-    skip_before_action :authorized
+	skip_before_action :authorized
 
-    def get_current_user
-        render json: current_user
-        # retrieve a token and decode that token in order to get current user
-    end
+	def get_current_user
+		render json: current_user
+		# retrieve a token and decode that token in order to get current user
+	end
 
-    def login   
-        @user = User.find_by(username: params[:username])
-        if @user && @user.authenticate(params[:password])
-            @token = encode_token({ user_id: @user.id })
-            render json: { user: @user, token: @token }, status: :ok
-        else
-            render json: { errors: ["Username and password doesn't match"] }, status: :unprocessable_entity
-        end
-    end
+	def login   
+		@user = User.find_by(username: params[:username])
+		if @user && @user.authenticate(params[:password])
+			@token = encode_token({ user_id: @user.id })
+			render json: { user: @user, token: @token }, status: :ok
+		else
+			render json: { errors: ["Username and password doesn't match"] }, status: :unprocessable_entity
+		end
+	end
 
 end
