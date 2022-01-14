dbfbf6194509b3da76e208827a1e07ee08276073
kaye garcia <kayemgarcia@gmail.com>
2022-01-14 10:50:25 -0600

    Edited profile page

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,4 +1,8 @@
 class Listing < ApplicationRecord
     has_many :reviews
     has_many :images
+
+    def self.most_reviews
+        self.all.max_by { |listing| listing.reviews.length }
+    end
 end
